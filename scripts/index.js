489ae0af0473f4049922b25cb111cc37f4720a24
msekhar12 <sekhar.mekala@gmail.com>
2022-09-02 14:25:44 -0500

    Sprint 4 project part 1

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,11 @@
 let initialCards = [
-  { name: "Yosemite Valley", link: "../images/yosemite.jpg" },
-  { name: "Lake Louise", link: "../images/lake-louise.jpg" },
-  { name: "Bald Mountains", link: "../images/bald-mountains.jpg" },
-  { name: "Latemar", link: "../images/latemar.jpg" },
-  { name: "Vanoise National Park", link: "../images/vanoise.jpg" },
-  { name: "Lago di Braies", link: "../images/lago.jpg " },
+  { name: "Yosemite Valley", link: "../images/yosemite-valley.jpg" },
+  { name: "Lake Louise", link: "../images/lake-louise.png" },
+  { name: "Bald Mountains", link: "../images/bald-mountains.png" },
+  { name: "Latemar", link: "../images/latemar.png" },
+  {
+    name: "Vanoise National Park",
+    link: "../images/vanoise-national-park.jpg",
+  },
+  { name: "Lago di Braies", link: "../images/lago-di-braies.png" },
 ];
