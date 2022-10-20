49e15eb216db9fa7050e7226fd145e29f14abc54
msekhar12 <sekhar.mekala@gmail.com>
2022-10-19 23:33:23 -0500

    Sprint 6 initial commit after some changes

--- scripts/index.js
+++ scripts/index.js
@@ -210,3 +210,59 @@ function handleCreateCardSubmit(event) {
 }
 
 addCardFormElement.addEventListener("submit", handleCreateCardSubmit);
+
+const showError = (evt, errorMessage) => {
+  const errorElement = document.querySelector(`#${evt.target.id}-error`);
+  errorElement.textContent = errorMessage;
+  errorElement.classList.add(`${evt.currentTarget["name"]}-error_display`);
+  console.log("In show error");
+  console.log(`${evt.currentTarget["name"]}_display`);
+};
+
+const removeError = (evt) => {
+  const errorElement = document.querySelector(`#${evt.target.id}-error`);
+  errorElement.classList.remove(`${evt.currentTarget["name"]}-error_display`);
+  errorElement.textContent = "";
+};
+
+const hasInValidInput = (inputList) => {
+  //Iterate over the array using "some" method
+  return inputList.some((inputElement) => {
+    return !inputElement.validity.valid;
+  });
+};
+
+const isValid = (evt) => {
+  if (evt.target.validity.valid) {
+    removeError(evt);
+  } else {
+    showError(evt, evt.target.validationMessage);
+  }
+
+  const formName = evt.currentTarget["name"];
+  const formContainer = formName.split("__")[0];
+
+  // select button using ID
+  // If ID is not used, then we will get empty object
+  // once the class is removed when the button is disabled or enabled
+  const button = document.querySelector("#" + formContainer + "__button");
+
+  const formElements = Array.from(evt.currentTarget.elements);
+
+  if (hasInValidInput(formElements)) {
+    button.setAttribute("disabled", true);
+    button.classList.add(formContainer + "__button-disabled");
+    button.classList.remove(formContainer + "__button");
+  } else {
+    button.removeAttribute("disabled");
+    button.classList.add(formContainer + "__button");
+    button.classList.remove(formContainer + "__button-disabled");
+  }
+};
+
+// Add "input" event listener at the form level
+// This will avoid the need to add input event listener
+// at the field level
+Array.from(document.forms).forEach((element) => {
+  element.addEventListener("input", isValid);
+});
